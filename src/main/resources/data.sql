c42303979be727db310502dc8cb7c60816272ad4
Nripendra Thakur <nrpndr@users.noreply.github.com>
2023-11-12 10:28:31 +0530

    Adding some events data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,41 @@
+INSERT IGNORE INTO `db_events`.`events` (
+	id, 
+	name, 
+	event_date, 
+	event_start_time, 
+	event_duration_in_minutes, 
+	venue, 
+	description,
+	artist_names
+	)
+VALUES (
+	1, 
+	'Comedy With Zakir',
+	'01/02/2024',
+	'05:00 PM',
+	'60',
+	'Shilparamam, Hitec City',
+	'A Fun filled evening with Zakir',
+	'Zakir Khan'
+	);
+
+INSERT IGNORE INTO `db_events`.`events` (
+	id, 
+	title, 
+	event_date, 
+	event_start_time, 
+	event_duration_in_minutes, 
+	venue, 
+	description,
+	artist_names
+	)
+VALUES (
+	2, 
+	'Badshah Beats',
+	'25/12/2023',
+	'05:00 PM',
+	'120',
+	'Boulder Hills, Gachibowli',
+	'Fun Filled Evening with Badshah and his super energetic songs',
+	'Badshah'
+	);
